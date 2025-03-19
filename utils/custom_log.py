a61d6b6a3401c036e432289964729acc311ae223
Beelzseals <restyanszkij@gmail.com>
2025-03-20 00:25:55 +0100

    enhance logging functionality by organizing log files into daily directories and adding a stream handler

--- utils/custom_log.py
+++ utils/custom_log.py
@@ -4,12 +4,15 @@ import os
 
 def create_logger():
     os.makedirs("logs", exist_ok=True)
+    current_day = datetime.datetime.now().strftime("%Y-%m-%d")
+    os.makedirs(os.path.join("logs", current_day), exist_ok=True)
     logger = logging.getLogger('logger')
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         handlers=[
-            logging.FileHandler(f'logs/{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")}.log'),
+            logging.FileHandler(f'logs/{current_day}/{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")}.log'),
+            logging.StreamHandler()
             ]
     )
     return logger

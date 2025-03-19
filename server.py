ed695bec069305a15fd1983649ecfc8b5f140f2e
Beelzseals <restyanszkij@gmail.com>
2025-03-20 00:38:18 +0100

    comment out image generation endpoints in server.py for future refactoring

--- server.py
+++ server.py
@@ -5,21 +5,21 @@ import uvicorn
 app = FastAPI()
 generator = ImageGenerator()
 
-@app.post("/generate/dalle")
-def generate_dalle_images(prompt: dict):
+# @app.post("/generate/dalle")
+# def generate_dalle_images(prompt: dict):
 
-    generator.generate_dalle_images(prompt)
-    return {"status": "success"}
+#     generator.generate_dalle_images(prompt)
+#     return {"status": "success"}
 
-@app.post("/generate/stable-diffusion")
-def generate_sd_images(prompt: dict):
-    generator.generate_hf_images("flux", prompt)
-    return {"status": "success"}
+# @app.post("/generate/stable-diffusion")
+# def generate_sd_images(prompt: dict):
+#     generator.generate_hf_images("sd", prompt)
+#     return {"status": "success"}
 
-@app.post("/generate/flux")
-def generate_flux_images(prompt: dict):
-    generator.generate_hf_images(prompt)
-    return {"status": "success"}
+# @app.post("/generate/flux")
+# def generate_flux_images(prompt: dict):
+#     generator.generate_hf_images("flux", prompt)
+#     return {"status": "success"}
 
 @app.post("/generate/all")
 def generate_all_images():

59fa264d1c9aec2393739a198e36158704b22adf
Beelzseals <restyanszkij@gmail.com>
2025-03-26 13:22:17 +0100

    refactor: encapsulate inpainting logic within Inpainter class and integrate OpenAI client

--- inpainter.py
+++ inpainter.py
@@ -1,55 +1,56 @@
-#TODO - CURRENTLY UNUSED, PLANNED FOR FUTURE VERSION
-
 import io
 import base64
-import openai
-from models.used_models import  load_inpainter
+from models.used_models import  load_inpainter, load_openai_client
 from PIL import Image
 
+class Inpainter:
+    def __init__(self):
+        self.sd_pipe = load_inpainter()
+        self.openai_client = load_openai_client()
+
+    def _b64_to_image(self,b64_str):
+        image_data = base64.b64decode(b64_str)
+        return Image.open(io.BytesIO(image_data)).convert("RGBA")
+
+    def _image_to_b64(self,img):
+        buffered = io.BytesIO()
+        img.save(buffered, format="PNG")
+        return base64.b64encode(buffered.getvalue()).decode()
+
+    def _mask_rgba_to_single_channel(self,mask):
+        return mask.convert("L")
 
 
-def b64_to_image(b64_str):
-    image_data = base64.b64decode(b64_str)
-    return Image.open(io.BytesIO(image_data)).convert("RGBA")
-
-def image_to_b64(img):
-    buffered = io.BytesIO()
-    img.save(buffered, format="PNG")
-    return base64.b64encode(buffered.getvalue()).decode()
-
-def mask_rgba_to_single_channel(mask):
-    return mask.convert("L")
+    def inpaint_with_sd(self,image: Image.Image, mask: Image.Image, prompt: str = "") -> Image.Image:
+        image = image.resize((512, 512))
+        mask = mask.resize((512, 512))
+        output = self.sd_pipe(prompt=prompt, image=image, mask_image=mask).images[0]
+        return output
 
 
-def stable_diffusion_inpaint(image: Image.Image, mask: Image.Image, prompt: str = "") -> Image.Image:
-    image = image.resize((512, 512))
-    mask = mask.resize((512, 512))
-    pipe = load_inpainter()
-    output = pipe(prompt=prompt, image=image, mask_image=mask).images[0]
-    return output
+    def inpaint_with_dalle(self, image: Image.Image, mask: Image.Image, prompt: str = "") -> Image.Image:
+        image = image.convert("RGB")
+        mask = mask.convert("L")
 
+        # Convert image and mask to base64
+        buffered = io.BytesIO()
+        image.save(buffered, format="PNG")
+        image_b64 = base64.b64encode(buffered.getvalue()).decode()
 
-def dalle2_inpaint(image: Image.Image, mask: Image.Image, prompt: str = "") -> Image.Image:
-    image = image.convert("RGB")
-    mask = mask.convert("L")
+        buffered_mask = io.BytesIO()
+        mask.save(buffered_mask, format="PNG")
+        mask_b64 = base64.b64encode(buffered_mask.getvalue()).decode()
 
-    # Convert image to base64
-    buffered = io.BytesIO()
-    image.save(buffered, format="PNG")
-    image_b64 = base64.b64encode(buffered.getvalue()).decode()
+        response = self.openai_client.images.edit(
+            image=image_b64,
+            mask=mask_b64,
+            prompt=prompt,
+            n=1,
+            size="512x512",
+            response_format="b64_json"
+        )
+        result_b64 = response["data"][0]["b64_json"]
+        return self._b64_to_image(result_b64)
 
-    buffered_mask = io.BytesIO()
-    mask.save(buffered_mask, format="PNG")
-    mask_b64 = base64.b64encode(buffered_mask.getvalue()).decode()
-
-    # OpenAI API Call
-    response = openai.images.edit(
-        image=image_b64,
-        mask=mask_b64,
-        prompt=prompt,
-        n=1,
-        size="512x512",
-        response_format="b64_json"
-    )
-    result_b64 = response["data"][0]["b64_json"]
-    return b64_to_image(result_b64)
+    # def inpaint_with_flux(self,image: Image.Image, mask: Image.Image, prompt: str = "") -> Image.Image:
+    #     pass

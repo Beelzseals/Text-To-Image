83f40c16145e9e3d6138287c8f3171906952f8e4
Beelzseals <restyanszkij@gmail.com>
2025-03-30 11:40:59 +0200

    feat: enhance Inpainter to initialize folders and save inpainted images by date and model

--- inpainter.py
+++ inpainter.py
@@ -1,4 +1,6 @@
 import io
+import os
+import datetime
 import base64
 from ml_models.model_manager import ModelManager
 from PIL import Image
@@ -6,11 +8,22 @@ from models.inpaint_model import InpaintModel
 
 
 class Inpainter:
-    def __init__(self):
+    def __init__(self, logger):
         model_manager = ModelManager()
+        self.current_day = datetime.datetime.now().strftime("%Y-%m-%d")
+        self.logger = logger
+        self._init_folders()
         self.sd_pipe = model_manager.get_pipeline("inpainting", "stable_diffusion", "sd_2_inpainting")
-        self.flux_pipe = None  # Currently not used
         self.openai_client = model_manager.load_openai_client()
+
+    def _init_folders(self):
+        inpaint_path = "images/inpaint"
+        os.makedirs("images", exist_ok=True)
+        os.makedirs(inpaint_path, exist_ok=True)
+
+        os.makedirs(os.path.join(inpaint_path, self.current_day), exist_ok=True)
+        os.makedirs(os.path.join(inpaint_path, self.current_day, "stable_diffusion_2"), exist_ok=True)
+        os.makedirs(os.path.join(inpaint_path, self.current_day, "dalle_2"), exist_ok=True)
 
     def _np_to_image(self, np_array):
         return Image.fromarray(np_array)
@@ -19,7 +32,7 @@ class Inpainter:
         image = image.resize((512, 512))
         mask = mask.resize((512, 512))
         output = self.sd_pipe(prompt=prompt, image=image, mask_image=mask).images[0]
-        return output
+        self.save_inpainted_image(output, "stable_diffusion_2")
 
     def _inpaint_with_dalle(self, image: Image.Image, mask: Image.Image, prompt: str = "") -> Image.Image:
         image = image.convert("RGB")
@@ -39,9 +52,14 @@ class Inpainter:
         )
         result_b64 = response["data"][0]["b64_json"]
         res_img = Image.frombytes("RGB", (512, 512), base64.b64decode(result_b64))
+        self.save_inpainted_image(res_img, "dalle_2")
 
-    # def inpaint_with_flux(self,image: Image.Image, mask: Image.Image, prompt: str = "") -> Image.Image:
-    #     pass
+    def save_inpainted_image(self, image: Image.Image, model: str):
+        model_path = os.path.join("images", "inpaint", self.current_day, model)
+        os.makedirs(model_path, exist_ok=True)
+        image_path = os.path.join(model_path, f"{model}_{self.current_day}.png")
+        image.save(image_path)
+        return image_path
 
     def inpaint_with_all_models(self, inpaint_inputs: InpaintModel):
         image = self._np_to_image(inpaint_inputs.image)
@@ -56,6 +74,7 @@ class Inpainter:
         mask = self._np_to_image(inpaint_inputs.mask)
         prompt = inpaint_inputs.prompt
         model = inpaint_inputs.model
+        result = None
 
         if model == "DALL-E":
             result = self._inpaint_with_dalle(image, mask, prompt)

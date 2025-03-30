777f21cbd55aee0e544bb9c2929ea933f7424574
Beelzseals <restyanszkij@gmail.com>
2025-03-30 11:39:44 +0200

    chore: change name of logger

--- utils/custom_logger.py
+++ utils/custom_logger.py
@@ -2,17 +2,18 @@ import logging
 import datetime
 import os
 
+
 def create_logger():
     os.makedirs("logs", exist_ok=True)
     current_day = datetime.datetime.now().strftime("%Y-%m-%d")
     os.makedirs(os.path.join("logs", current_day), exist_ok=True)
-    logger = logging.getLogger('logger')
+    logger = logging.getLogger("logger")
     logging.basicConfig(
         level=logging.INFO,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         handlers=[
-            logging.FileHandler(f'logs/{current_day}/{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")}.log'),
-            logging.StreamHandler()
-            ]
+            logging.FileHandler(f"logs/{current_day}/{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')}.log"),
+            logging.StreamHandler(),
+        ],
     )
     return logger
